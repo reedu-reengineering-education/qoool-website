62aef3353f43190d4c64ba2d02fea0ad204e5f68
Felix Erdmann <hello@felixerdmann.com>
2024-04-10 15:23:42 +0200

    feat: add people about

--- src/components/pages/about/people.tsx
+++ src/components/pages/about/people.tsx
@@ -3,30 +3,40 @@ export const reeduPeople = [
     id: 1,
     name: "Sergey Mukhametov",
     image: "https://reedu.de/assets/img/team/sergey_new.jpg",
+    about:
+      "Sergey leitet das QOOOL-Projekt und ist Co-Founder von re:edu. Er ist im QOOOL-Projekt für die Konzeption und Umsetzung des QOOOL-Kits verantwortlich.",
   },
   {
     id: 2,
     name: "Artur Kim Shum",
     designation: "Innovation Designer",
     image: "https://reedu.de/assets/img/team/KIMSHUM.png",
+    about:
+      "Kim ist Architekt und Innovationsdesigner und arbeitet seit 2021 im Bereich Design Thinking und Innovationsmanagement bei re:edu. Er ist im QOOOL-Projekt für die Konzeption und Umsetzung des QOOOL-Kits verantwortlich.",
   },
   {
     id: 3,
     name: "Gina Buchwald-Chassée",
     designation: "Public Relations",
     image: "https://reedu.de/assets/img/team/gina_new.jpg",
+    about:
+      "Gina hat ihr Bachelor- und Masterstudium im Fach Kommunikationswissenschaft an der Universität Münster absolviert und arbeitet seit 2021 im Bereich Öffentlichkeitsarbeit/Kommunikation bei re:edu. Sie ist im QOOOL-Projekt für die Betreuung der Webseite und Social Media zuständig.",
   },
   {
     id: 4,
     name: "Felix Erdmann",
     designation: "Software Developer",
     image: "https://reedu.de/assets/img/team/felix_new.jpg",
+    about:
+      "Felix ist Software Developer und kümmert sich im QOOOL-Projekt um die Entwicklung der Webseite und des Simulators. ✌️",
   },
   {
     id: 5,
     name: "Umut Taş",
     designation: "Software Developer",
     image: "https://reedu.de/assets/img/team/umut_new.jpg",
+    about:
+      "Umut ist Co-Founter und Software Developer bei re:edu und kümmert sich im QOOOL-Projekt um die Entwicklung der Webseite und des Simulators.",
   },
 ]
 
@@ -36,6 +46,8 @@ export const ifmPeople = [
     name: "André Bülau",
     designation: "Dr. Elektrotechnik",
     image: require("@/assets/images/partners/ifm/andre.JPG").default.src,
+    about:
+      "André Bülau hat seinen Abschluss in Elektrotechnik 2007 von der Universität Magdeburg, Deutschland, mit dem Fokus auf Informationstechnologie und Mikrosystemtechnik erhalten. Im Jahr 2024 hat er seine Doktorprüfung erfolgreich abgelegt. Seit 2007 arbeitet er bei Hahn-Schickard am Institut für Mikroaufbautechnik in Stuttgart in der Gruppe Sensoren und Aktoren und ist dort Gruppenleiter. Im Projekt fungiert er als Konsortialführung und Projektleitung und befasst sich mit der Entwicklung des QOOOL Kits.",
   },
   {
     id: 7,
@@ -43,6 +55,8 @@ export const ifmPeople = [
     designation: "Medizintechnik",
     image: require("@/assets/images/partners/ifm/Daniela_Walter.jpg").default
       .src,
+    about:
+      "Daniela Walter hat ihren Abschluss in Medizintechnik mit dem Fokus auf Mikrosystemtechnologie und Signalverarbeitung von der Universität Stuttgart, Deutschland im Jahr 2018 erhalten. Seit 2019 arbeitet sie bei Hahn-Schickard am Institut für Mikroaufbautechnik in Stuttgart in der Gruppe Sensoren und Aktoren und ist dort stellvertretende Gruppenleitung. Im Projekt fungiert Sie als Stellvertretung in der Konsortialführung und Projektleitung und befasst sich mit der Entwicklung des QOOOL Kits.",
   },
 ]
 
@@ -53,6 +67,8 @@ export const iisPeople = [
     designation: "Institutsleiter",
     image: require("@/assets/images/partners/iis/Bild_Jens_Anders.jpg").default
       .src,
+    about:
+      "Jens Anders erhielt den MSc Abschluss von der University of Michigan, Ann Arbor sowie den Dipl.-Ing. von der Leibniz Universität Hannover jeweils in Elektrotechnik in den Jahren 2005 bzw. 2007. Im Jahr 2011 erhielt er den Doktorgrad von der École Polytechnique Fédérale de Lausanne (EPFL). Von 2013 bis 2017 war er Juniorprofessor für Biomedizinische Integrierte Sensoren am Institut für Mikroelektronik der Universität Ulm. Seit 2017 ist er Inhaber des Lehrstuhls für „Elektrotechnik Bionischer Systeme“ und Direktor des Instituts für Intelligente Sensorik an der Universität Stuttgart. Seit 2022 ist er zudem auch Direktor am Institut für Mikroelektronik Stuttgart (IMS CHIPS). Er konzeptioniert und leitet alle Aufgaben des IIS in Rahmen des QOOOL-Sensing Projekts.",
   },
   {
     id: 9,
@@ -60,11 +76,15 @@ export const iisPeople = [
     designation: "PhD Physical Chemistry",
     image: require("@/assets/images/partners/iis/Bild_Michal_Kern.png").default
       .src,
+    about:
+      "Michal Kern erhielt den MSc Abschluss von der Brno University of Technology, Czechien, im Bereich Physikalische Ingenieurwesen und Nanotechnologien. Im Jahr 2021 erhielt er den Doktorgrad von der Universität Stuttgart in Physikalische Chemie. Derzeit ist er Akademischer Rat und Gruppenleiter der ‚Magnetische Resonanz und Spintronik‘ Gruppe am IIS. Er hilft bei der Konzeptionierung der benötigten Mikrowellentechnik, sowie bei der Umsetzung des Konzeptes im Labor.",
   },
   {
     id: 10,
     name: "Yichao Peng",
     designation: "MSc. Elektrotechnik & Informationstechnik",
     image: require("@/assets/images/partners/iis/Yichao Peng.jpg").default.src,
+    about:
+      "Yichao Peng erhielt seinen MSc Titel von der Universität Stuttgart in Elektrotechnik und Informationstechnik im Jahr 2023. Er hat sich schon in seiner Masterarbeit mit der Entwicklung von integrierten Quantensensorsystemen beschäftigt und wird im Rahmen seiner Promotion das QOOOL-Kit mitentwickeln.",
   },
 ]
