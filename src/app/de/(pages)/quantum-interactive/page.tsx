cae8531304f676706791ed63037963779d8cf648
Felix Erdmann <hello@felixerdmann.com>
2025-12-02 12:06:56 +0100

    fix: update fonts

--- src/app/de/(pages)/quantum-interactive/page.tsx
+++ src/app/de/(pages)/quantum-interactive/page.tsx
@@ -1,88 +1,92 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import H1 from "@/components/ui/typography/H1"
+import H4 from "@/components/ui/typography/H4"
+import P from "@/components/ui/typography/P"
 import IframeResizer from "@iframe-resizer/react"
 import { Download } from "lucide-react"
 import Link from "next/link"
 
 export default function LearnModulesQuizPage() {
   return (
-    <div className="grid gap-6">
-      <h3 className="font-semibold">
+    <div>
+      <H1>Quanten Interaktiv</H1>
+      <P>
         In diesem Bereich werden interaktive Lern- und Experimentiermaterialien
         zum Thema Quantensensorik bereitgestellt. Die Angebote reichen von
         Quizformaten über Versuchsanleitungen bis hin zu digitalen Lernmodulen:
         ein Ort zum Entdecken, Erforschen und Verstehen von Quantenphänomenen
-      </h3>
-      <div className="rounded bg-white overflow-hidden text-black">
-        <div className="p-4">
-          <h4 className="font-semibold">NV-Zentren verstehen</h4>
-          <p>
-            Was passiert im Inneren eines Diamanten, wenn eine Leerstelle
-            entstehen? Das interaktive Quiz führt Schritt für Schritt in die
-            Grundlagen der NV-Zentren ein und vermittelt auf anschauliche Weise,
-            wie aus winzigen Gitterfehlern in Diamanten die Grundlage für
-            Quantensensoren entsteht.
-          </p>
+      </P>
+      <div className="grid gap-12 pt-8">
+        <div className="rounded bg-white overflow-hidden text-black">
+          <div className="p-4">
+            <h4 className="font-semibold">NV-Zentren verstehen</h4>
+            <p>
+              Was passiert im Inneren eines Diamanten, wenn eine Leerstelle
+              entstehen? Das interaktive Quiz führt Schritt für Schritt in die
+              Grundlagen der NV-Zentren ein und vermittelt auf anschauliche
+              Weise, wie aus winzigen Gitterfehlern in Diamanten die Grundlage
+              für Quantensensoren entsteht.
+            </p>
+          </div>
+          <IframeResizer
+            log="collapsed"
+            src="https://reedu-reengineering-education.github.io/qoool-nv-quiz/NV_To_Go.html"
+            style={{ width: "100%", height: "100vh" }}
+            license={"GPLv3"}
+            allowFullScreen
+            allow="fullscreen; geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor"
+          />
         </div>
-        <IframeResizer
-          log="collapsed"
-          src="https://reedu-reengineering-education.github.io/qoool-nv-quiz/NV_To_Go.html"
-          style={{ width: "100%", height: "100vh" }}
-          license={"GPLv3"}
-          allowFullScreen
-          allow="fullscreen; geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor"
-        />
-      </div>
-      <div className="grid gap-1">
-        <h4 className="font-semibold">
-          Experimentieranleitung: QOOOL Kit Fluoro
-        </h4>
-        <p>
-          Mit der senseBox und dem QOOOL Kit Fluoro lässt sich das Phänomen der
-          Fluoreszenz praktisch erforschen. Die Anleitung bietet
-          Hintergrundwissen, Aufbauhinweise und einfache Auswertungen für
-          Schule, Schülerlabor und Ausbildung: ideal, um die physikalischen
-          Grundlagen der Lichtemission sichtbar zu machen.
-        </p>
-        <Link
-          href={
-            "/QOOOL-Kit-Fluoro_Experimentieranleitung_Hintergrundwissen.pdf"
-          }
-        >
-          <Button className="w-fit">
-            <Download className="h-4 mr-2" />
-            Experimentieranleitung
-          </Button>
-        </Link>
-      </div>
-      <div className="grid gap-1">
-        <h4 className="font-semibold">
-          Arbeitsblatt: Der quantenmechanische Potentialtopf
-        </h4>
-        <p>
-          In diesen frei verfügbaren Dokumenten liegt flexibel
-          zusammenstellbares Material für einen Unterrichtsgang zum
-          Potentialtopf bereit. Von kostengünstigen Lehrer- und SuS-Experimenten
-          (einmalig ca. 40 Euro), über Erklärtexte, Abbildungen, Übungsaufgaben
-          und Musterlösungen. Alles ist erprobt und frei zugänglich. Inhaltlich
-          ist das Lehrplanthema an quantenmechanisch gefärbten Salzen
-          aufgezogen, deren robuste und deutliche Farbe ist mit dem Model des
-          Potentialtopfes sogar quantitativ für SuS erreichbar.
-        </p>
-        <Link href={"/AB_Potentialtopf_Simon_v3.pdf"}>
-          <Button className="w-fit">
-            <Download className="h-4 mr-2" />
-            Arbeitsblatt Potentialtopf
-          </Button>
-        </Link>
-        <div className="h-1" />
-        <Link href={"/Potentialtopf_Experimentieranleitung.pdf"}>
-          <Button className="w-fit">
-            <Download className="h-4 mr-2" />
-            Experimentieranleitung Potentialtopf
-          </Button>
-        </Link>
+        <div className="grid gap-2">
+          <H4>Experimentieranleitung: QOOOL Kit Fluoro</H4>
+          <P>
+            Mit der senseBox und dem QOOOL Kit Fluoro lässt sich das Phänomen
+            der Fluoreszenz praktisch erforschen. Die Anleitung bietet
+            Hintergrundwissen, Aufbauhinweise und einfache Auswertungen für
+            Schule, Schülerlabor und Ausbildung: ideal, um die physikalischen
+            Grundlagen der Lichtemission sichtbar zu machen.
+          </P>
+          <Link
+            href={
+              "/QOOOL-Kit-Fluoro_Experimentieranleitung_Hintergrundwissen.pdf"
+            }
+          >
+            <Button className="w-fit" size={"sm"}>
+              <Download className="h-4 mr-2" />
+              Experimentieranleitung
+            </Button>
+          </Link>
+        </div>
+        <div className="grid gap-2">
+          <H4>Arbeitsblatt: Der quantenmechanische Potentialtopf</H4>
+          <P>
+            In diesen frei verfügbaren Dokumenten liegt flexibel
+            zusammenstellbares Material für einen Unterrichtsgang zum
+            Potentialtopf bereit. Von kostengünstigen Lehrer- und
+            SuS-Experimenten (einmalig ca. 40 Euro), über Erklärtexte,
+            Abbildungen, Übungsaufgaben und Musterlösungen. Alles ist erprobt
+            und frei zugänglich. Inhaltlich ist das Lehrplanthema an
+            quantenmechanisch gefärbten Salzen aufgezogen, deren robuste und
+            deutliche Farbe ist mit dem Model des Potentialtopfes sogar
+            quantitativ für SuS erreichbar.
+          </P>
+          <div className="grid gap-4">
+            <Link href={"/AB_Potentialtopf_Simon_v3.pdf"}>
+              <Button className="w-fit" size={"sm"}>
+                <Download className="h-4 mr-2" />
+                Arbeitsblatt Potentialtopf
+              </Button>
+            </Link>
+            <Link href={"/Potentialtopf_Experimentieranleitung.pdf"}>
+              <Button className="w-fit" size={"sm"}>
+                <Download className="h-4 mr-2" />
+                Experimentieranleitung Potentialtopf
+              </Button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/app/en/(pages)/quantum-interactive/page.tsx
+++ src/app/en/(pages)/quantum-interactive/page.tsx
@@ -2,13 +2,16 @@
 
 import { Alert, AlertTitle } from "@/components/ui/alert"
 import { Button } from "@/components/ui/button"
+import H1 from "@/components/ui/typography/H1"
+import H4 from "@/components/ui/typography/H4"
+import P from "@/components/ui/typography/P"
 import IframeResizer from "@iframe-resizer/react"
 import { Download, InfoIcon } from "lucide-react"
 import Link from "next/link"
 
 export default function LearnModulesQuizPage() {
   return (
-    <div className="grid gap-6">
+    <div>
       <Alert>
         <InfoIcon className="pr-2" />
         <AlertTitle className="font-normal leading-normal">
@@ -18,78 +21,80 @@ export default function LearnModulesQuizPage() {
         </AlertTitle>
       </Alert>
 
-      <h3 className="font-semibold">
+      <H1>Quantum Interactive</H1>
+      <P>
         In this area, interactive learning and experimental materials on the
         topic of Quantum Sensing are provided. The offerings range from quiz
         formats and experiment instructions to digital learning modules: a place
         to discover, explore, and understand quantum phenomena.
-      </h3>
-      <div className="rounded bg-white overflow-hidden text-black">
-        <div className="p-4">
-          <h4 className="font-semibold">Understanding NV-Centers</h4>
-          <p>
-            What happens inside a diamond when a vacancy is created? This
-            interactive quiz introduces the fundamentals of NV-centers step by
-            step and clearly conveys how tiny lattice defects in diamonds become
-            the foundation for quantum sensors.
-          </p>
+      </P>
+      <div className="grid gap-12 pt-8">
+        <div className="rounded bg-white overflow-hidden text-black">
+          <div className="p-4">
+            <h4 className="font-semibold">Understanding NV-Centers</h4>
+            <p>
+              What happens inside a diamond when a vacancy is created? This
+              interactive quiz introduces the fundamentals of NV-centers step by
+              step and clearly conveys how tiny lattice defects in diamonds
+              become the foundation for quantum sensors.
+            </p>
+          </div>
+          <IframeResizer
+            log="collapsed"
+            src="https://reedu-reengineering-education.github.io/qoool-nv-quiz/NV_To_Go.html"
+            style={{ width: "100%", height: "100vh" }}
+            license={"GPLv3"}
+            allowFullScreen
+            allow="fullscreen; geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor"
+          />
         </div>
-        <IframeResizer
-          log="collapsed"
-          src="https://reedu-reengineering-education.github.io/qoool-nv-quiz/NV_To_Go.html"
-          style={{ width: "100%", height: "100vh" }}
-          license={"GPLv3"}
-          allowFullScreen
-          allow="fullscreen; geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor"
-        />
-      </div>
-      <div className="grid gap-1">
-        <h4 className="font-semibold">Experiment Guide: QOOOL Kit Fluoro</h4>
-        <p>
-          With the senseBox and the QOOOL Kit Fluoro, the phenomenon of
-          fluorescence can be practically explored. The guide provides
-          background knowledge, setup instructions, and simple evaluation
-          methods for schools, student labs, and training: ideal for visualizing
-          the physical fundamentals of light emission.
-        </p>
-        <Link
-          href={
-            "/QOOOL-Kit-Fluoro_Experimentieranleitung_Hintergrundwissen.pdf"
-          }
-        >
-          <Button className="w-fit">
-            <Download className="h-4 mr-2" />
-            Experiment Guide
-          </Button>
-        </Link>
-      </div>
-      <div className="grid gap-1">
-        <h4 className="font-semibold">
-          Worksheet: The Quantum Mechanical Potential Well
-        </h4>
-        <p>
-          These freely available documents contain flexible, modular material
-          for a lesson sequence on the potential well. This includes
-          cost-effective experiments for teachers and students (approx. 40 Euros
-          one-time cost), explanatory texts, diagrams, practice exercises, and
-          sample solutions. Everything is tested and freely accessible. The
-          curriculum topic is based on quantum-mechanically colored salts, whose
-          robust and distinct color can even be quantitatively explained to
-          students using the potential well model.
-        </p>
-        <Link href={"/AB_Potentialtopf_Simon_v3.pdf"}>
-          <Button className="w-fit">
-            <Download className="h-4 mr-2" />
-            Potential Well Worksheet
-          </Button>
-        </Link>
-        <div className="h-1" />
-        <Link href={"/Potentialtopf_Experimentieranleitung.pdf"}>
-          <Button className="w-fit">
-            <Download className="h-4 mr-2" />
-            Potential Well Experiment Guide
-          </Button>
-        </Link>
+        <div className="grid gap-2">
+          <H4>Experiment Guide: QOOOL Kit Fluoro</H4>
+          <P>
+            With the senseBox and the QOOOL Kit Fluoro, the phenomenon of
+            fluorescence can be practically explored. The guide provides
+            background knowledge, setup instructions, and simple evaluation
+            methods for schools, student labs, and training: ideal for
+            visualizing the physical fundamentals of light emission.
+          </P>
+          <Link
+            href={
+              "/QOOOL-Kit-Fluoro_Experimentieranleitung_Hintergrundwissen.pdf"
+            }
+          >
+            <Button className="w-fit" size={"sm"}>
+              <Download className="h-4 mr-2" />
+              Experiment Guide
+            </Button>
+          </Link>
+        </div>
+        <div className="grid gap-2">
+          <H4>Worksheet: The Quantum Mechanical Potential Well</H4>
+          <P>
+            These freely available documents contain flexible, modular material
+            for a lesson sequence on the potential well. This includes
+            cost-effective experiments for teachers and students (approx. 40
+            Euros one-time cost), explanatory texts, diagrams, practice
+            exercises, and sample solutions. Everything is tested and freely
+            accessible. The curriculum topic is based on quantum-mechanically
+            colored salts, whose robust and distinct color can even be
+            quantitatively explained to students using the potential well model.
+          </P>
+          <div className="grid gap-4">
+            <Link href={"/AB_Potentialtopf_Simon_v3.pdf"}>
+              <Button className="w-fit" size={"sm"}>
+                <Download className="h-4 mr-2" />
+                Potential Well Worksheet
+              </Button>
+            </Link>
+            <Link href={"/Potentialtopf_Experimentieranleitung.pdf"}>
+              <Button className="w-fit" size={"sm"}>
+                <Download className="h-4 mr-2" />
+                Potential Well Experiment Guide
+              </Button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   )
