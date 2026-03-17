ca7c9233083ee30b4edf8cf5444ee628eaef464f
Felix Erdmann <hello@felixerdmann.com>
2026-03-17 17:26:48 +0100

    fix: build error

--- src/app/de/(pages)/quantum-interactive/diy/page.tsx
+++ src/app/de/(pages)/quantum-interactive/diy/page.tsx
@@ -1,8 +1,13 @@
 "use client"
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function LearnModulesQuizPage() {
   const router = useRouter()
-  router.replace("/de/quantum-interactive/diy/quenching")
+
+  useEffect(() => {
+    router.replace("/de/quantum-interactive/diy/quenching")
+  }, [router])
+
   return null
 }
--- src/app/de/(pages)/quantum-interactive/page.tsx
+++ src/app/de/(pages)/quantum-interactive/page.tsx
@@ -1,9 +1,14 @@
 "use client"
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function LearnModulesQuizPage() {
   // replace to /de/quantum-interactive/lernmaterialien
   const router = useRouter()
-  router.replace("/de/quantum-interactive/lernmaterialien")
+
+  useEffect(() => {
+    router.replace("/de/quantum-interactive/lernmaterialien")
+  }, [router])
+
   return null
 }
